95f60babeaedc72a55fa174e5fce7cc1bc4f62e3
ObedSAGA <obedsg95@gmail.com>
2022-08-11 19:07:36 +0200

    Add description in CardProduct

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { FC, useMemo, useState } from 'react';
-import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import NextLink from 'next/link';
+import { Box, Card, CardActionArea, CardMedia, Grid, Link, Typography } from '@mui/material';
 
 import { IProduct } from '../../interfaces';
 
@@ -25,13 +26,18 @@ export const ProductCard: FC<Props> = ({ product }) => {
             onMouseLeave={() => setIsHovered(false)}
         >
             <Card>
-                <CardActionArea>
-                    <CardMedia
-                        component='img'
-                        image={productImage}
-                        alt={product.title}
-                    />
-                </CardActionArea>
+                <NextLink href="/product/slug" passHref prefetch={false}>
+                    <Link>    
+                        <CardActionArea>
+                            <CardMedia
+                                component='img'
+                                className='fadeIn'
+                                image={productImage}
+                                alt={product.title}
+                            />
+                        </CardActionArea>
+                    </Link>
+                </NextLink>
             </Card>
 
             <Box sx={{ mt: 1 }} className='fadeIn'>
