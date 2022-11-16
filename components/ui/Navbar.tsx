daa71637238f03fff0714b73275501eb9a5aa4d5
ObedSAGA <obedsg95@gmail.com>
2022-11-16 21:13:12 +0100

    Update badge in Navbar with number of items from state

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -5,13 +5,14 @@ import NextLink from "next/link";
 import { AppBar, Badge, Box, Button, IconButton, Input, InputAdornment, Link, Toolbar, Typography } from '@mui/material';
 import { ClearOutlined, SearchOutlined, ShoppingCartOutlined } from "@mui/icons-material";
 
-import { UiContext } from "../../context";
+import { CartContext, UiContext } from "../../context";
 
 
 export const Navbar = () => {
 
     const { asPath, push } = useRouter();
     const { toggleSideMenu } = useContext(UiContext);
+    const { numberOfItems } = useContext(CartContext);
 
     const [searchTerm, setSearchTerm] = useState('');
     const [isSearchVisible, setIsSearchVisible] = useState(false)
@@ -114,7 +115,7 @@ export const Navbar = () => {
                 <NextLink href='/cart' passHref>
                     <Link>
                         <IconButton>
-                            <Badge badgeContent={2} color='secondary'>
+                            <Badge badgeContent={ numberOfItems > 9 ? '+9' : numberOfItems} color='secondary'>
                                 <ShoppingCartOutlined />
                             </Badge>
                         </IconButton>
