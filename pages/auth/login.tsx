5bed25b5b081212db7591d58d903dae4c9ea9b8e
ObedSAGA <obedsg95@gmail.com>
2022-11-23 19:23:13 +0100

    Add React-Hook-Form to Login form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -3,38 +3,70 @@ import NextLink from 'next/link';
 import { Grid, Typography, TextField, Button, Link } from '@mui/material';
 import { Box } from '@mui/system';
 import { AuthLayout } from '../../components/layouts'
+import { useForm } from 'react-hook-form';
+
+
+type FormData = {
+    email: string,
+    password: string,
+};
 
 const LoginPage = () => {
-  return (
-    <AuthLayout title='Login'>
-        <Box sx={{ width: 350, padding: '10px 20px'}}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant='h1' component='h1'>Iniciar sesión</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField label='Correo electrónico' variant='filled' fullWidth/>
-                </Grid>
 
-                <Grid item xs={12}>
-                    <TextField label='Constraseña' variant='filled' type='password' fullWidth/>
-                </Grid>
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
-                <Grid item xs={12}>
-                    <Button variant='contained' color='secondary' fullWidth>Login</Button>
-                </Grid>
+    const onLoginUser = (data: FormData) => {
+        console.log({ data });
 
-                <Grid item xs={12} display='flex' justifyContent='center'>
-                    <Typography sx={{ mr: 1}}>¿No tienes cuenta?</Typography>
-                    <NextLink href='/auth/register' passHref>
-                        <Link underline='always'>Regístrate.</Link>
-                    </NextLink>
-                </Grid>
-            </Grid>
-        </Box>
+    }
+    return (
+        <AuthLayout title='Login'>
+            <form onSubmit={handleSubmit(onLoginUser)}>
+                <Box sx={{ width: 350, padding: '10px 20px' }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component='h1'>Iniciar sesión</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                type='email'
+                                label='Correo electrónico' 
+                                variant='filled' 
+                                fullWidth 
+                                { ...register('email')}
+                            />
+                        </Grid>
 
-        
-    </AuthLayout>
+                        <Grid item xs={12}>
+                            <TextField 
+                                label='Constraseña' 
+                                variant='filled' 
+                                type='password' 
+                                fullWidth 
+                                { ...register('password')}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button
+                                type='submit'
+                                variant='contained'
+                                color='secondary'
+                                fullWidth>
+                                Login
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12} display='flex' justifyContent='center'>
+                            <Typography sx={{ mr: 1 }}>¿No tienes cuenta?</Typography>
+                            <NextLink href='/auth/register' passHref>
+                                <Link underline='always'>Regístrate.</Link>
+                            </NextLink>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </form>
+        </AuthLayout>
     )
 }
 
