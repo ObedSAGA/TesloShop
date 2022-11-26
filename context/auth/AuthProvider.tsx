632d548b5d6b906eb15c07499d6363ced562c670
ObedSAGA <obedsg95@gmail.com>
2022-11-26 13:06:35 +0100

    Add AuthContext

---
+++ context/auth/AuthProvider.tsx
@@ -0,0 +1,34 @@
+import { FC, ReactNode , useReducer } from 'react';
+import { IUser } from '../../interfaces';
+import { AuthContext, authReducer } from './';
+
+export interface AuthState {
+  isLoggedIn: boolean;
+  user?: IUser;
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+  isLoggedIn: false,
+  user: undefined,
+};
+
+interface Props {
+   children?: ReactNode;
+}
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+
+        // Methods
+
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};      
