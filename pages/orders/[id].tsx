c8f3cbcdcf4e32f61ebeeed1a85fe7d29399688f
ObedSAGA <obedsg95@gmail.com>
2022-12-09 13:51:51 +0100

    Add funtion tu find order by id in data base and get data for page confimation page

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -1,81 +1,138 @@
+import { GetServerSideProps, NextPage } from 'next'
 import NextLink from 'next/link';
+import { getSession } from 'next-auth/react';
 import { Box, Card, CardContent, Chip, Divider, Grid, Link, Typography } from '@mui/material';
 import { CartList, OrderSummary } from '../../components/cart';
 import { ShopLayout } from '../../components/layouts';
 import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material';
+import { dbOrders } from '../../database';
+import { IOrder } from '../../interfaces';
 
 
-const OrderPage = () => {
-  return (
-    <ShopLayout title='Confirmación de pedido 4964644545' pageDescription='Confirmación de pedido a pagar'>
-        <Typography variant='h1' component='h1'>Pedido: 51515</Typography>
-        
-        {/* <Chip
+interface Props {
+    order: IOrder
+}
+
+
+const OrderPage: NextPage<Props> = ({ order }) => {
+
+    console.log({ order });
+
+    return (
+        <ShopLayout title='Confirmación de pedido 4964644545' pageDescription='Confirmación de pedido a pagar'>
+            <Typography variant='h1' component='h1'>Pedido: 51515</Typography>
+
+            {/* <Chip
             sx={{ my: 2 }}
             label='Pendiente de pago'
             variant='outlined'
             color='error'
             icon={ <CreditCardOffOutlined/>}
         /> */}
-        <Chip
-            sx={{ my: 2 }}
-            label='Pago realizado'
-            variant='outlined'
-            color='success'
-            icon={ <CreditScoreOutlined/>}
-        />
-        
-        <Grid container>
-            <Grid item xs={12} sm={7}>
-                <CartList />
+            <Chip
+                sx={{ my: 2 }}
+                label='Pago realizado'
+                variant='outlined'
+                color='success'
+                icon={<CreditScoreOutlined />}
+            />
+
+            <Grid container>
+                <Grid item xs={12} sm={7}>
+                    <CartList />
+                </Grid>
+                <Grid item xs={12} sm={5}>
+                    <Card className='summary-card'>
+                        <CardContent>
+                            <Typography variant='h2'>Resumen</Typography>
+                            <Divider sx={{ my: 1 }} />
+
+                            <Box display='flex' justifyContent='space-between'>
+                                <Typography variant='subtitle1'>Dirección de entrega</Typography>
+                                <NextLink href='/checkout/address' passHref>
+                                    <Link underline='always'>
+                                        Editar
+                                    </Link>
+                                </NextLink>
+                            </Box>
+
+                            <Typography>Alberto Herrera</Typography>
+                            <Typography>C/ Hortes 76 2-1</Typography>
+                            <Typography>CP: 08005 Barcelona, España </Typography>
+                            <Typography>+34 622 90 90 90</Typography>
+
+                            <Divider sx={{ my: 1 }} />
+
+                            <Box display='flex' justifyContent='end'>
+                                <NextLink href='/cart' passHref>
+                                    <Link underline='always'>
+                                        Editar
+                                    </Link>
+                                </NextLink>
+                            </Box>
+
+
+                            <OrderSummary />
+
+                            <Box sx={{ mt: 3 }}>
+                                {/* TODO:  */}
+                                <h1>Pagar</h1>
+
+                            </Box>
+
+
+                        </CardContent>
+                    </Card>
+
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={5}>
-                <Card className='summary-card'>
-                    <CardContent>
-                        <Typography variant='h2'>Resumen</Typography>
-                        <Divider sx={{ my: 1}}/>
-
-                        <Box display='flex' justifyContent='space-between'>
-                            <Typography variant='subtitle1'>Dirección de entrega</Typography>
-                            <NextLink href='checkout/address' passHref>
-                                <Link underline='always'>
-                                    Editar
-                                </Link>
-                            </NextLink>
-                        </Box>
-
-                        <Typography>Alberto Herrera</Typography>
-                        <Typography>C/ Hortes 76 2-1</Typography>
-                        <Typography>CP: 08005 Barcelona, España </Typography>
-                        <Typography>+34 622 90 90 90</Typography>
-
-                        <Divider sx={{ my: 1}}/>
-
-                        <Box display='flex' justifyContent='end'>
-                            <NextLink href='/cart' passHref>
-                                <Link underline='always'>
-                                    Editar
-                                </Link>
-                            </NextLink>
-                        </Box>
+        </ShopLayout>
+    )
+}
 
 
-                        <OrderSummary/>
-                        
-                        <Box sx={{ mt: 3}}>
-                            {/* TODO:  */}
-                            <h1>Pagar</h1>
-                            
-                        </Box>
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
+    const { id = '' } = query
+    const session: any = await getSession({ req });
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: `/auth/login?p=/orders/${id}`,
+                permanent: false,
+            }
+        }
+    }
+
+    const order = await dbOrders.getOrderById(id.toString());
+
+    if (!order) {
+        return {
+            redirect: {
+                destination: '/orders/history',
+                permanent: false,
+            }
+        }
+    }
+
+    if (order.user !== session.user.id) {
+        return {
+            redirect: {
+                destination: '/orders/history',
+                permanent: false,
+            }
+        }
+    }
 
 
-                    </CardContent>
-                </Card>
-
-            </Grid>
-        </Grid>
-    </ShopLayout>
-  )
+    return {
+        props: {
+            order
+        }
+    }
 }
 
 export default OrderPage;
